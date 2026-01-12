a261715af6b2a49463dab1c86563291b43576269
aiwenzmg <2366401708@qq.com>
2026-01-12 15:56:13 +0800

    添加子部门的表单结构

--- src/views/department/components/add-dept.vue
+++ src/views/department/components/add-dept.vue
@@ -1,5 +1,28 @@
 <template>
-  <el-dialog title="新增部门" :visible="showDialog" @close="close" />
+  <el-dialog title="新增部门" :visible="showDialog" @close="close">
+    <el-form label-width="120px">
+      <el-form-item label="部门名称">
+        <el-input placeholder="2-10个字符" style="width: 80%;" size="mini" />
+      </el-form-item>
+      <el-form-item label="部门编码">
+        <el-input placeholder="2-10个字符" style="width: 80%;" size="mini" />
+      </el-form-item>
+      <el-form-item label="部门负责人">
+        <el-select placeholder="请选择负责人" style="width: 80%;" size="mini" />
+      </el-form-item>
+      <el-form-item label="部门介绍">
+        <el-input placeholder="1-100个字符" :rows="4" style="width: 80%;" size="mini" />
+      </el-form-item>
+      <el-form-item>
+        <el-row type="flex" justify="center">
+          <el-col :span="12">
+            <el-button type="primary" size="mini">确认</el-button>
+            <el-button size="mini">取消</el-button>
+          </el-col>
+        </el-row>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
 </template>
 <script>
 export default {
