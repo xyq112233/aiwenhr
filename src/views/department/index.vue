8cf4b4ffa0d24af7163d01b231906b49f78bf318
aiwenzmg <2366401708@qq.com>
2026-01-12 11:02:48 +0800

    树型组件的应用

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,12 +1,37 @@
 <template>
   <div class="container">
     <div class="app-container">
-      组织架构
+      <!-- 展示树型结构 -->
+      <el-tree :default-expand-all="true" :data="depts" :props="defaultProps" />
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'Department'
+  name: 'Department',
+  data() {
+    return {
+      depts: [{
+        name: '传智教育',
+        children: [{
+          name: '总裁办'
+        }, {
+          name: '行政部'
+        }, {
+          name: '人事部'
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  }
 }
 </script>
+<style scoped>
+.app-container{
+  padding: 30px 140px;
+  font-size: 14px;
+}
+</style>
