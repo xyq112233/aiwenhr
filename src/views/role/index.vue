a0488635c8738f63342235585416411c9ffc8853
aiwenzmg <2366401708@qq.com>
2026-01-13 09:10:49 +0800

    角色管理页面结构

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="container">
     <div class="app-container">
-      角色管理
+      <!-- 角色管理内容 -->
+      <div class="role-operate">
+        <el-button type="primary" size="mini">添加角色</el-button>
+      </div>
+      <!-- 放置table组件 -->
+      <el-table>
+        <el-table-column align="center" label="角色" width="200" />
+        <el-table-column align="center" label="启用" width="200" />
+        <el-table-column align="center" label="描述" />
+        <el-table-column align="center" label="操作" />
+      </el-table>
+      <!-- 放置分页组件 -->
+      <el-row type="flex" style="height: 60px;" align="middle" justify="end">
+        <el-pagination layout="prev, pager, next" />
+      </el-row>
     </div>
   </div>
 </template>
@@ -10,3 +24,8 @@ export default {
   name: 'Role'
 }
 </script>
+<style scoped>
+.role-operate{
+  padding: 10px;
+}
+</style>
