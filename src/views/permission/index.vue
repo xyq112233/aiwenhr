fa2353432c5be77e7d08319ff0ef97154c994161
aiwenzmg <2366401708@qq.com>
2026-01-14 16:48:14 +0800

    权限管理-获取权限数据

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,12 +1,45 @@
 <template>
   <div class="container">
     <div class="app-container">
-      权限管理
+      <el-button class="btn-add" size="mini" type="primary">添加权限</el-button>
+      <el-table :data="permissionList" row-key="id" :default-expand-all="true">
+        <el-table-column prop="name" label="名称" />
+        <el-table-column prop="code" label="标识" />
+        <el-table-column prop="description" label="描述" />
+        <el-table-column label="操作">
+          <template v-slot="{row}">
+            <el-button v-if="row.type === 1 " size="mini" type="text">添加</el-button>
+            <el-button size="mini" type="text">编辑</el-button>
+            <el-button size="mini" type="text">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 <script>
+import { getPermissionList } from '@/api/permission'
+import { transListToTreeData } from '@/utils'
 export default {
-  name: 'Permission'
+  name: 'Permission',
+  data() {
+    return {
+      permissionList: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async getPermissionList() {
+      const res = await getPermissionList()
+      this.permissionList = transListToTreeData(res, 0)
+    }
+  }
 }
 </script>
+<style>
+.btn-add {
+  margin: 10px;
+}
+</style>
