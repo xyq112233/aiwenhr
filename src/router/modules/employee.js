03976ccef1d851c35b5feed7f0a8d2a8ceac9f33
aiwenzmg <2366401708@qq.com>
2026-01-14 09:38:40 +0800

    员工详情的页面结构

--- src/router/modules/employee.js
+++ src/router/modules/employee.js
@@ -10,5 +10,12 @@ export default {
       title: '员工',
       icon: 'people'
     }
+  }, {
+    path: '/employee/detail',
+    component: () => import('@/views/employee/detail'),
+    hidden: true, // 隐藏路由
+    meta: {
+      title: '员工详情'
+    }
   }]
 }
